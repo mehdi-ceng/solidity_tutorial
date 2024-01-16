69b040b283045bf30afeec856fc5a9e70a683843
Mehdi Maharramov <mehdi.maharramov1@gmail.com>
2024-01-16 11:50:34 +0400

    Delete Lesson4/v1.0/PriceConverter.sol

--- Lesson4/v1.0/PriceConverter.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-
-//Libraries can't have state variables, they can't send ether
-//All functions of library will be internal
-library PriceConverter{
-
-}
